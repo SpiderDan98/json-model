9bcb37599f7d8a8611559828bde5308e34aadc19
SpiderDan98 <40319023+SpiderDan98@users.noreply.github.com>
2024-02-23 01:42:52 +0100

    feat: add casts (#1)

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,26 @@
-export declare type ObjectKey = string | number;
+export declare type ObjectKey = string | number | symbol;
 
-export interface Config {
+export declare type CastFunction = (value: unknown) => unknown;
+export declare type DefaultCasts = "date" | "string" | "int" | "float";
+
+export interface Config<CastKeys extends ObjectKey = ObjectKey> {
+  casts: Partial<Record<CastKeys, DefaultCasts | CastFunction>>;
   models: Record<string, Model<any>>;
   ignoreAttributes: ObjectKey[];
   resolveModelName: <M extends object>(model: M) => ObjectKey;
   resolveRelationshipNames: <M extends object>(model: M) => ObjectKey[];
 }
 
-export declare type ConfigDefinition =
-  | ((config: Config) => Config)
-  | Partial<Config>;
+export declare type ConfigDefinition<CastKeys extends ObjectKey = ObjectKey> =
+  | ((config: Config<CastKeys>) => Config<CastKeys>)
+  | Partial<Config<CastKeys>>;
 
 export interface Model<Output extends object = {}> {
-  new (data: Record<string, unknown>, localConfig?: ConfigDefinition): Output;
+  new (data: object, localConfig?: ConfigDefinition<keyof Output>): Output;
 
-  create<
-    T extends object,
-    Data extends Record<string, unknown> | Record<string, unknown>[] = {}
-  >(
+  create<T extends object, Data extends object | object[] = {}>(
     this: new (...args: any[]) => T,
     data: Data,
-    localConfig?: ConfigDefinition
+    localConfig?: ConfigDefinition<keyof Output>
   ): Data extends object[] ? (Output & T)[] : Output & T;
 }
